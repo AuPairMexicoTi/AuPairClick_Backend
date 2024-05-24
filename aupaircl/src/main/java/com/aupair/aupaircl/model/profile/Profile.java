03e1bc2c3b3e6a8bf5a562d10135f8f2ff880d9c
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-05-24 13:56:01 -0600

    [Add] Model Profile

--- aupaircl/src/main/java/com/aupair/aupaircl/model/profile/Profile.java
+++ aupaircl/src/main/java/com/aupair/aupaircl/model/profile/Profile.java
@@ -1,4 +1,69 @@
 package com.aupair.aupaircl.model.profile;
 
+import com.aupair.aupaircl.model.country.Country;
+import com.aupair.aupaircl.model.gender.Gender;
+import com.aupair.aupaircl.model.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.PrePersist;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "apc_profiles")
 public class Profile {
+
+    @Id
+    @Column(name = "id_profile",nullable = false)
+    @GeneratedValue(generator = "UUID")
+    private UUID profileId;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_user", nullable = false)
+    private User user;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_gender", nullable = false)
+    private Gender gender;
+
+    @Column(name = "age",nullable = false)
+    private Integer age;
+    @Column(name = "nationality",nullable = false)
+    private String nationality;
+    @Column(name = "country_of_residence",nullable = false)
+    private String countryOfResidence;
+    @Column(name = "languages_spoken",nullable = false)
+    private String languagesSpoken;
+    @Column(name = "about_me",nullable = false)
+    private String aboutMe;
+    @Column(name = "min_stay_months",nullable = false)
+    private Integer minStayMonths;
+    @Column(name = "max_stay_months",nullable = false)
+    private Integer maxStayMonths;
+    @ManyToOne
+    @JoinColumn(name = "fk_country", nullable = false)
+    private Country country;
+
+    @PrePersist
+    private void generateUUID(){
+        if(this.profileId==null){
+            this.profileId = UUID.randomUUID();
+        }
+    }
 }
