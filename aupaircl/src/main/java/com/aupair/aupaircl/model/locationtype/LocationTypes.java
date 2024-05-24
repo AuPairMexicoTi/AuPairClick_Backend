a7ab05ee0611d81cf62f6495ebef4758217c01e3
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-05-24 13:55:08 -0600

    [Add] Model LocationTypes

--- aupaircl/src/main/java/com/aupair/aupaircl/model/locationtype/LocationTypes.java
+++ aupaircl/src/main/java/com/aupair/aupaircl/model/locationtype/LocationTypes.java
@@ -1,4 +1,35 @@
 package com.aupair.aupaircl.model.locationtype;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.PrePersist;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "apc_location_types")
 public class LocationTypes {
+
+
+    @Id
+    @Column(name = "id_location_type",nullable = false)
+    @GeneratedValue(generator = "UUID")
+    private UUID locationTypeId;
+    @Column(name = "location_type_name",nullable = false, unique = true)
+    private String locationTypeName;
+    @PrePersist
+    private void generateUUID(){
+        if(this.locationTypeId==null){
+            this.locationTypeId= UUID.randomUUID();
+        }
+    }
 }
