4fa7b44360d9480d85ec9c87df07b9cf58ecce5a
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-05-24 13:55:26 -0600

    [Add] Model Messages

--- aupaircl/src/main/java/com/aupair/aupaircl/model/message/Messages.java
+++ aupaircl/src/main/java/com/aupair/aupaircl/model/message/Messages.java
@@ -1,4 +1,53 @@
 package com.aupair.aupaircl.model.message;
 
+import com.aupair.aupaircl.model.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "apc_messages")
 public class Messages {
+
+    @Id
+    @Column(name = "id_messages",nullable = false)
+    @GeneratedValue(generator = "UUID")
+    private UUID messageId;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_sender", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_receiver", nullable = false)
+    private User receiver;
+
+    @Column(name = "contenct",nullable = false)
+    private String content;
+
+    private Date sentAt;
+    @PrePersist
+    private void generateUUID(){
+        if(this.sentAt==null){
+            this.sentAt= new Date();
+        }
+        if(this.messageId==null){
+            this.messageId = UUID.randomUUID();
+        }
+    }
 }
+
