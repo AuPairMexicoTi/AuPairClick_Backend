a2a29cfa4e72f73aefecc5603871e457e07e4172
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-05-24 13:56:40 -0600

    [Add] Model RequestStatus

--- aupaircl/src/main/java/com/aupair/aupaircl/model/requeststatus/RequestStatus.java
+++ aupaircl/src/main/java/com/aupair/aupaircl/model/requeststatus/RequestStatus.java
@@ -1,4 +1,37 @@
 package com.aupair.aupaircl.model.requeststatus;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "status")
+@Entity
 public class RequestStatus {
+    @Id
+    @Column(name = "id_status", nullable = false)
+    @GeneratedValue(generator = "UUID")
+    private UUID idStatus;
+
+    @Column(name = "status_name",nullable = false)
+    private String statusName;
+
+    @Column(name = "status_description",nullable = false)
+    private String statusDescription;
+    @PrePersist
+    private void generateUUID() {
+        if (this.idStatus == null) {
+            this.idStatus = UUID.randomUUID();
+        }
+    }
 }
